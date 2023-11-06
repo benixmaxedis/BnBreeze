4086b6fa619c88f0cef34d0112dd34bdbd09122e
benixmaxedis <benwales@bigpond.com>
2023-11-07 08:42:43 +1000

    Updated logo from logomaker

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -13,7 +13,7 @@ export default function Logo() {
       className="hidden md:block cursor-pointer"
       height="100"
       width="100"
-      src="/images/logo2.jpg"
+      src="/images/logo.png"
     />
   );
 }
